7a3ff543dbadb56571496f3e4e6f6fb964d38489
Rocio Alvarez <alvarezroci1998@gmail.com>
2025-08-05 16:09:46 -0300

    voy a llorar

--- javascript/index.js
+++ javascript/index.js
@@ -5,13 +5,13 @@ const section = document.querySelector("section.misProd");
 const misProd = albumes.map(
   (album) =>
     `<div class="card" style="width: 300px">
-    <img src="../medios/${album.id}.jpg" class="card-img-top" alt="..." />
+    <img src="./medios/${album.id}.jpg" class="card-img-top" alt="..." />
     <div class="card-body">
       <h5 class="card-title">${album.name} - ${album.group}</h5>
       <p class="card-text">
 					${album.description}
 				</p>
-      <a href="./producto.html?id=${album.id}" class="btn btn-primary">
+      <a href="../producto.html?id=${album.id}" class="btn btn-primary">
         Más información
       </a>
     </div>
