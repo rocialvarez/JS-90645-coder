f7370f01db18d2fcfb35323a926661d7731c97ea
Rocio Alvarez <alvarezroci1998@gmail.com>
2025-08-16 13:47:18 -0300

    Carrito de compras mejorado con bootstrap

--- javascript/albumes.js
+++ javascript/albumes.js
@@ -5,7 +5,7 @@ const albumes = [
     group: "Enhypen",
     description:
       "Mini álbum debut del grupo surcoreano ENHYPEN. Incluye 6 canciones, entre ellas el exitoso sencillo Given-Taken. Disponible en versiones Dusk y Dawn.",
-    price: "25000",
+    price: "$25000",
   },
   {
     id: 2,
@@ -13,7 +13,7 @@ const albumes = [
     group: "Enhypen",
     description:
       "Primer álbum de estudio de ENHYPEN, lanzado el 12 de octubre de 2021. Consta de 8 canciones, incluida la poderosa Tamed‑Dashed. Disponible en múltiples versiones visuales como Scylla, Odysseus y Charybdis.",
-    price: "31000",
+    price: "$31000",
   },
   {
     id: 3,
@@ -21,7 +21,7 @@ const albumes = [
     group: "Enhypen",
     description:
       "Cuarto mini‑álbum (EP) del grupo, lanzado el 22 de mayo de 2023. Con 6 temas, liderado por el sencillo Bite Me. Inspirado en el concepto vampírico de su webtoon Dark Moon: The Blood Altar, ofrece un sonido cohesivo, oscuro y poderoso.",
-    price: "21000",
+    price: "$21000",
   },
   {
     id: 4,
@@ -29,7 +29,7 @@ const albumes = [
     group: "Enhypen",
     description:
       "Segundo álbum de estudio (también su tercer álbum coreano), lanzado el 12 de julio de 2024. Incluye 9 canciones, con el éxito XO (Only If You Say Yes) como sencillo principal. Un viaje sonoro entre pop, R&B y synth-pop inspirado en un romance vampírico bajo la luna. ",
-    price: "35000",
+    price: "$35000",
   },
 ];
 
--- javascript/main.js
+++ javascript/main.js
@@ -1,28 +1,28 @@
-const button = document.querySelector("button");
-const input = document.querySelector("input");
-const error = document.querySelector(".bg-danger");
-const ul = document.querySelector("ul");
+const button = document.querySelector('button');
+const input = document.querySelector('input');
+const error = document.querySelector('.bg-danger');
+const ul = document.querySelector('ul');
 
-button.addEventListener("click", handleClick);
-input.addEventListener("keydown", handleHide);
+button.addEventListener('click', handleClick);
+input.addEventListener('keydown', handleHide);
 
 function handleClick() {
-  if (input.value === "") {
-    error.classList.remove("d-none");
+  if (input.value === '') {
+    error.classList.remove('d-none');
     return;
   }
-  const li = document.createElement("li");
-  const span = document.createElement("span");
-  const borrar = document.createElement("img");
-  borrar.src = "../medios/borrar-opcion.png";
+  const li = document.createElement('li');
+  const span = document.createElement('span');
+  const borrar = document.createElement('img');
+  borrar.src = '../medios/borrar-opcion.png';
   borrar.onclick = () => li.remove();
   span.textContent = input.value;
   li.appendChild(span);
   li.appendChild(borrar);
   ul.appendChild(li);
-  input.value = "";
+  input.value = '';
 }
 
 function handleHide() {
-  error.classList.add("d-none");
+  error.classList.add('d-none');
 }
