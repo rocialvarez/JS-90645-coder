82942ccc62d3f374f1519df8e040c27b5dbe58d9
Rocio Alvarez <alvarezroci1998@gmail.com>
2025-08-18 12:35:24 -0300

    Notificación de Sweetalert para mensaje de error

--- javascript/main.js
+++ javascript/main.js
@@ -3,25 +3,48 @@ const input = document.querySelector('input');
 const error = document.querySelector('.bg-danger');
 const ul = document.querySelector('ul');
 
+const carrito = JSON.parse(localStorage.getItem('carrito')) || [];
+
 button.addEventListener('click', handleClick);
 input.addEventListener('keydown', handleHide);
 
 function handleClick() {
   if (input.value === '') {
-    error.classList.remove('d-none');
+    Swal.fire({
+      icon: 'error',
+      title: '¡Un momento!',
+      text: 'Por favor ingresa número mayor a 0',
+      confirmButtonText: 'Aceptar',
+    });
     return;
   }
+
   const li = document.createElement('li');
   const span = document.createElement('span');
   const borrar = document.createElement('img');
+
+  const total = album.price * input.value;
+
   borrar.src = '../medios/borrar-opcion.png';
   borrar.onclick = () => li.remove();
+  li.textContent = `${album.name} - $${total}`;
   span.textContent = input.value;
   li.appendChild(span);
   li.appendChild(borrar);
   ul.appendChild(li);
   input.value = '';
 }
+
+carrito.push({
+  name: album.name,
+  price: album.price,
+  quantity: Number(input.value),
+  total: total,
+});
+
+localStorage.setItem('carrito', JSON.stringify(carrito));
+
+input.value = '';
 
 function handleHide() {
   error.classList.add('d-none');
